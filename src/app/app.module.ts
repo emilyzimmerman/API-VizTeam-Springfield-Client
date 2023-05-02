9385ee54f55a82cd3aab7dd2463f54978fa2a5fc
sarahdclayton81@gmail.com <sarahdclayton81@gmail.com>
2023-05-01 20:30:26 -0500

    add: signup component & signup-modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignupModalComponent } from './signup-modal/signup-modal.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignupComponent,
+    SignupModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
