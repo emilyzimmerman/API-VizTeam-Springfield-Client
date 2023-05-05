f0f3f0bb031664d43ad837356f4e82a5feaf7b47
emilyzimmerman <zimmermanemily121@gmail.com>
2023-05-04 20:22:27 -0500

    Added Auto Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../shared/models/user.model';
+import { UserService } from './user.service';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  showHome = false;
+
+  constructor(private http: HttpClient, private userService: UserService) {}
 
   login(user: any) {
     return this.http.post('http://localhost:3000/api/v1/users/login', user);
+  }
+
+  autoSignIn() {
+    //get token from browser (already declared by getToken())
+    const token = this.getToken();
+    if (!token) {
+      //go through normal flow (see sign in / login)
+      return;
+    }
+
+    this.http
+      .get('http://localhost:3000/api/v1/users/me', {
+        headers: {
+          Authorization: `Bearer ${token.value}`,
+        },
+      })
+      .subscribe((res: any) => {
+        if (res.success) {
+          console.log(res.payload.user);
+          this.userService.setCurrentUser(res.payload.user);
+          //set showHome to true so it shows home component
+          this.showHome = true;
+        }
+      });
+    //send request to get user info
+  }
+  //get token --> have to parse from JSON to typescript
+  getToken() {
+    return JSON.parse(localStorage.getItem('token'));
+  }
+
+  setToken(token) {
+    localStorage.setItem('token', JSON.stringify(token));
+  }
+
+  removeToken() {
+    localStorage.removeItem('token');
   }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -26,9 +26,10 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     const loginUser = this.loginForm.value;
     this.authService.login(loginUser).subscribe((res: any) => {
-      if (res.sucess) {
+      if (res.success) {
         //if true, then set current user
         this.userService.setCurrentUser(res.payload.user);
+        this.authService.setToken(res.payload.token);
       }
       console.log(res);
     });
