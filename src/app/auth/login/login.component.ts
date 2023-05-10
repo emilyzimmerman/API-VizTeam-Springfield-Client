2573be0b911db49def799c5918feea4dfd30c2de
emilyzimmerman <zimmermanemily121@gmail.com>
2023-05-09 22:39:35 -0500

    Updated Login Form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { UserService } from '../user.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import { UserService } from '../user.service';
 })
 export class LoginComponent implements OnInit {
   buttonDisabled = false;
-
+  error: boolean = false;
   loginForm = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
@@ -18,20 +19,25 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private userService: UserService
+    private userService: UserService,
+    private dialogRef: MatDialogRef<LoginComponent>
   ) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
     const loginUser = this.loginForm.value;
-    this.authService.login(loginUser).subscribe((res: any) => {
-      if (res.success) {
+    this.authService.login(loginUser).subscribe({
+      next: (res: any) => {
         //if true, then set current user
         this.userService.setCurrentUser(res.payload.user);
         this.authService.setToken(res.payload.token);
-      }
-      console.log(res);
+        this.dialogRef.close();
+      },
+      error: (errorRes) => {
+        console.error('An error occurred', errorRes);
+        this.error = true;
+      },
     });
   }
 }
